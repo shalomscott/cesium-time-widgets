ec409e1bed9f1e1826864f7fa47ed5f66e2ad49b
Shalom Steinbach <shalomss525@gmail.com>
2020-09-29 16:09:18 +0300

    remove menu

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -4,12 +4,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  menuItems = this.router.config.filter(route => route.data?.menu).map(route => route.path);
-
   constructor(private router: Router) {
-
+    this.router.navigateByUrl('cesium');
   }
 }
